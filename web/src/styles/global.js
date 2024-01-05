2df7b5c3818de7dd67e54ba738e13eb22cae6961
cVitorAndrade <vitorandrade1804@gmail.com>
2024-01-04 22:45:51 -0300

    initial commit

---
+++ web/src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        font-family: 'Noto Sans', sans-serif;
+        /* font-family: 'Montserrat', sans-serif; */
+    }
+
+    :root {
+        font-size: 62.5%;
+    }
+
+    body {
+        font-size: 1.6rem;
+    }
+`;
---
+++ web/src/styles/theme.js
@@ -0,0 +1,9 @@
+export default {
+    COLORS: {
+        BLACK: "#333333",
+        GREEN: "#3DB46D",
+        GRAY: "#BDBDBD",
+        RED: "##EB5757",
+        WHITE: "#FFFFFF"
+    }
+}
