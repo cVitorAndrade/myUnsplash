54b3c6e8783f76f3709e0897bd54b5e9cede3775
cVitorAndrade <vitorandrade1804@gmail.com>
2024-01-06 19:33:10 -0300

    feat: add CORS to allow API access

--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,15 @@ require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
 const AppError = require("./utils/AppError");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(routes);
+
 
 app.use((error, request, response, next) => {
     if ( error instanceof AppError ) {
