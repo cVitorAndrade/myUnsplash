cae307987648901120c0664f14cf2866372d7177
cVitorAndrade <vitorandrade1804@gmail.com>
2024-01-14 00:15:38 -0300

    feat: add authentication hook

---
+++ web/src/hooks/auth.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+const AuthContext = createContext({});
+
+export function AuthProvider ({ children }) {
+    const [data, setData] = useState({});
+
+    async function signIn ({ email, password }) {
+        try {
+            const response = await api.post("/sessions", { email, password });
+            const { user, token } = response.data;
+
+            localStorage.setItem("@myUnsplash:token", token);
+            localStorage.setItem("@myUnsplash:user", JSON.stringify(user))
+
+            api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+            setData({ user, token })
+
+        } catch (error) {
+            if ( error.response ) {
+                alert(error.response.data.message)
+            } else {
+                alert("Não foi possivél fazer login.")
+            }
+        }
+    }
+
+    useEffect(() => {
+        const user = localStorage.getItem("@myUnsplash:user");
+        const token = localStorage.getItem("@myUnsplash:token");
+
+        if ( user && token ) {
+            api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+            setData({
+                user: JSON.parse(user),
+                token
+            })
+        }
+    }, []);
+    
+    return(
+        <AuthContext.Provider 
+            value={{
+                signIn,
+                user: data.user
+            }}
+        >
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth () {
+    const context = useContext(AuthContext);
+    return context;
+}
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -6,13 +6,18 @@ import theme from './styles/theme'
 
 import GlobalStyle from './styles/global'
 
+import { AuthProvider } from './hooks/auth'
+
 import { Routes } from './routes'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
